5d7c79da48682e71bb4d5b2dac211d94dabdcfd2
asia Wang <gsboy00@163.com>
2015-04-13 21:24:00 +0800

    Initial Commit

---
+++ voice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  voice
+//
+//  Created by Asia wang on 15/4/13.
+//  Copyright (c) 2015年 Asia wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ voice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  voice
+//
+//  Created by Asia wang on 15/4/13.
+//  Copyright (c) 2015年 Asia wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
